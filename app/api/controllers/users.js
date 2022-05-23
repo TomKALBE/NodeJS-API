635880e41fa2ddc7d9096ae490b691050bed3940
Tom <t.kalbe@h3hitema.fr>
2022-05-23 16:27:15 +0200

    Ajout du test d'inscription

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -3,11 +3,14 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 module.exports = {
     create: function(req, res, next) {
-        userModel.create({ name: req.body.name, email: req.body.email, password: req.body.password }, function (err, result) {
+        const data = { name: req.body.name, email: req.body.email, password: req.body.password }
+        userModel.create(data, function (err, result) {
             if (err) 
                 next(err);
-            else
-                res.json({status: "success", message: "User added successfully!!!", data: null});
+            else{
+                res.status(201)
+                res.json({status: "success",name: req.body.name, email: req.body.email});
+            }
         });
     },
     authenticate: function(req, res, next) {
