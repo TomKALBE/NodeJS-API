70b1ab04cefff9e8aff0518c32a435db8af33cf9
Tom <t.kalbe@h3hitema.fr>
2022-05-23 15:54:23 +0200

    Ajout de l'authentification et de la route users/authenticate

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -10,4 +10,19 @@ module.exports = {
                 res.json({status: "success", message: "User added successfully!!!", data: null});
         });
     },
+    authenticate: function(req, res, next) {
+        userModel.findOne({email:req.body.email}, function(err, userInfo){
+            if (err) {
+                next(err);
+            } else {
+                if(bcrypt.compareSync(req.body.password, userInfo.password)) {
+                    const token = jwt.sign({id: userInfo._id}, req.app.get('secretKey'), { expiresIn: '1h' });
+                    res.json({status:"success", message: "user found!!!", data:{user: userInfo, token:token}});
+                }else{
+                    res.status(401)
+                    res.json({status:"error", message: "Invalid email/password!!!", data:null});
+                }
+            }
+        });
+    }
 }
